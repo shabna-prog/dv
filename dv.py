dd741f7122eb9a41157e9c35b5611f4e4f8f716e
shabna-prog <84712106+shabna-prog@users.noreply.github.com>
2025-11-27 12:35:59 +0530

    Update dv.py

--- dv.py
+++ dv.py
@@ -8,6 +8,7 @@ import streamlit as st
 '''Load model'''
 
 model=joblib.load(open("linear_regression_model.joblib",'rb'))
+st.write("Model expects features:", model.n_features_in_)
 st.title("Prediction")
 MPG=st.number_input("MPG",min_value=0.0)
 VOL=st.number_input("VOL",min_value=0.0)
@@ -19,3 +20,4 @@ if st.button('predict sles'):
     input_data=np.array([[MPG,VOL,SP,WT]])
     prediction=model.predict(input_data)[0]
     st.success(f'predict sles:{prediction:.2f}')
+

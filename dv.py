ba552b8dd19a92827fad1af610b9e214c0c7ce89
shabna-prog <84712106+shabna-prog@users.noreply.github.com>
2025-11-27 09:18:19 +0530

    Add files via upload

---
+++ dv.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import joblib
+import streamlit as st
+
+'''Load model'''
+
+model=joblib.load(open("linear_regression_model.joblib",'rb'))
+st.title("Prediction")
+MPG=st.number_input("MPG",min_value=0)
+VOL=st.number_input("VOL",min_value=0)
+SP=st.number_input("SP",min_value=0)
+WT=st.number_input("WT",min_value=0)
+
+'''Mke pred'''
+if st.button('predict sles'):
+    input_data=np.array([[MPG,VOL,SP,WT]])
+    prediction=model.predict(input_data)[0]
+    st.success(f"predict sles:{prediction:.2f}")
